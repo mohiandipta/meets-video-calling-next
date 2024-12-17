bbdad87545ea45159666d9cea308ab658bbdfeb0
mohiandipta <mohianmostofa@gmail.com>
2024-12-17 17:20:35 +0600

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,28 @@
+const sidebarLinks = [
+    {
+        label: 'Home',
+        route: '/',
+        imgUrl: '/icons/home.svg'
+    },
+    {
+        label: 'Upcoming',
+        route: '/upcoming',
+        imgUrl: '/icons/upcoming.svg'
+    },
+    {
+        label: 'Prevoius',
+        route: '/previous',
+        imgUrl: '/icons/previous.svg'
+    },
+    {
+        label: 'Recording',
+        route: '/recordings',
+        imgUrl: '/icons/recording.svg'
+    },
+    {
+        label: 'Personal Room',
+        route: '/personal-room',
+        imgUrl: '/icons/personal.svg'
+    }
+    
+]
